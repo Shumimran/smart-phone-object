6b967b44d8c713c96c004a68c96d6ce5bb6a7768
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-03 11:16:46 +0500

    Add files via upload
    Auther: shumaila imran

---
+++ main.js
@@ -0,0 +1,13 @@
+// Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand,
+//  model, and other key features like how much storage it has, the size of its screen, and how long its battery 
+//  lasts.
+var smartphone = {
+    Make: "Realme",
+    Model: "RMX1941",
+    Space: {
+        Storage: "32 GB",
+        Screensize: "6.00 inches",
+        Betterylife: "12 hours"
+    }
+};
+console.log(smartphone);
